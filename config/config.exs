815682715a50a0d681113fc0f7f4f1bbab00dfb2
Damjack <giacomello.damiano@gmail.com>
2018-04-10 10:11:46 +0200

    Fix mix and config logger

--- config/config.exs
+++ config/config.exs
@@ -16,15 +16,4 @@ use Mix.Config
 #
 #     Application.get_env(:exftp, :key)
 #
-# You can also configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
-
-# It is also possible to import configuration files, relative to this
-# directory. For example, you can emulate configuration per environment
-# by uncommenting the line below and defining dev.exs, test.exs and such.
-# Configuration from the imported file will override the ones defined
-# here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+config :logger, level: :info
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Exftp.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger, :ssh, :public_key, :crypto]
     ]
   end
 
