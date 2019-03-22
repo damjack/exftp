10fe604272f35280f5a4f3fa56aaec9f7d5bfc50
damjack <giacomello.damiano@gmail.com>
2019-03-22 16:23:03 +0100

    Add mock to test

--- mix.exs
+++ mix.exs
@@ -7,22 +7,29 @@ defmodule Exftp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A simple FTP/SFTP Elixir library",
+      name: "exftp"
     ]
   end
 
   # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger, :ssh, :public_key, :crypto]
-    ]
+    [extra_applications: [
+      :logger, :ssh, :public_key, :crypto
+    ]]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
+    [{:mock, "~> 0.3.0", only: :test}]
+  end
+
+  defp package do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      maintainers: ["Damiano Giacomello"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/damjack/exftp"}
     ]
   end
 end
