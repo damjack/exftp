c16b022fcbce9b042faf85a76d6f01b1581c6b36
damjack <giacomello.damiano@gmail.com>
2019-04-28 16:35:18 +0200

    Update mix

--- mix.exs
+++ mix.exs
@@ -1,16 +1,20 @@
 defmodule Exftp.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :exftp,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
-      deps: deps(),
       description: "A simple FTP/SFTP Elixir library",
-      name: "exftp",
-      package: package()
+      deps: deps(),
+      package: package(),
+      # Docs
+      name: "Exftp",
+      docs: docs()
     ]
   end
 
@@ -23,7 +27,10 @@ defmodule Exftp.MixProject do
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
-    [{:mock, "~> 0.3.0", only: :test}]
+    [
+      {:mock, "~> 0.3.0", only: :test},
+      {:ex_doc, "~> 0.19", only: :docs}
+    ]
   end
 
   defp package do
@@ -31,6 +38,21 @@ defmodule Exftp.MixProject do
       maintainers: ["Damiano Giacomello"],
       licenses: ["MIT"],
       links: %{github: "https://github.com/damjack/exftp"}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Exftp",
+      source_ref: "v#{@version}",
+      groups_for_modules: [
+        Exftp,
+        Exftp.Connection,
+        Exftp.Files,
+        Exftp.Directory,
+        Exftp.Helper,
+        Exftp.Error
+      ]
     ]
   end
 end
